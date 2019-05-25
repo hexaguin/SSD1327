4e30f3bcd2a67fbb43c439857fb5368c9fa10b29
Ryan Hattie <hexaguin@gmail.com>
2019-05-25 15:43:01 -0600

    Made header use tabs for consistancy

--- src/SSD1327.h
+++ src/SSD1327.h
@@ -6,33 +6,33 @@
 //#include "font8x8_basic.h"
 
 class SSD1327 {
-  public:
-    SSD1327(int cs, int dc, int rst);
-    void writeCmd(uint8_t reg);
-    void writeData(uint8_t data);
-    void setWriteZone(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2);
-    int coordsToAddress(uint8_t x, uint8_t y);
-    void drawPixel(uint8_t x, uint8_t y, uint8_t color, bool display);
-    void drawRect(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, uint8_t color, bool display = false);
-    void drawHLine(int x, int y, int length, uint8_t color, bool display = false);
-    void drawVLine(int x, int y, int length, uint8_t color, bool display = false);
-    void drawByteAsRow(uint8_t x, uint8_t y, uint8_t byte, uint8_t color);
-    void drawChar(uint8_t x, uint8_t y, char thisChar, uint8_t color);
-    void drawChar16(uint8_t x, uint8_t y, char thisChar, uint8_t color);
-    void drawChar32(uint8_t x, uint8_t y, char thisChar, uint8_t color);
-    void drawCharArray(uint8_t x, uint8_t y, char text[], uint8_t color, int size=8);
-    void drawString(uint8_t x, uint8_t y, String textString, uint8_t color, int size=8);
-    void fillStripes(uint8_t offset);
-    void clearBuffer();
-    void writeFullBuffer();
-    void setContrast(uint8_t contrast);
-    void initRegs();
-    void init();
-  private:
-    uint8_t frameBuffer[8192]; //Should mirror the display's own frameBuffer.
-    int _cs;
-    int _dc;
-    int _rst;
+	public:
+		SSD1327(int cs, int dc, int rst);
+		void writeCmd(uint8_t reg);
+		void writeData(uint8_t data);
+		void setWriteZone(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2);
+		int coordsToAddress(uint8_t x, uint8_t y);
+		void drawPixel(uint8_t x, uint8_t y, uint8_t color, bool display);
+		void drawRect(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, uint8_t color, bool display = false);
+		void drawHLine(int x, int y, int length, uint8_t color, bool display = false);
+		void drawVLine(int x, int y, int length, uint8_t color, bool display = false);
+		void drawByteAsRow(uint8_t x, uint8_t y, uint8_t byte, uint8_t color);
+		void drawChar(uint8_t x, uint8_t y, char thisChar, uint8_t color);
+		void drawChar16(uint8_t x, uint8_t y, char thisChar, uint8_t color);
+		void drawChar32(uint8_t x, uint8_t y, char thisChar, uint8_t color);
+		void drawCharArray(uint8_t x, uint8_t y, char text[], uint8_t color, int size=8);
+		void drawString(uint8_t x, uint8_t y, String textString, uint8_t color, int size=8);
+		void fillStripes(uint8_t offset);
+		void clearBuffer();
+		void writeFullBuffer();
+		void setContrast(uint8_t contrast);
+		void initRegs();
+		void init();
+	private:
+		uint8_t frameBuffer[8192]; //Should mirror the display's own frameBuffer.
+		int _cs;
+		int _dc;
+		int _rst;
 };
 
 #endif
